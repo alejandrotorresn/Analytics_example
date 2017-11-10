f6c5e750f68da48e27f950bbf6309ed1e59766cd
Luis Alejandro Torres <alejandrotorresn@gmail.com>
2017-11-09 22:34:24 -0500

    Update

---
+++ customer1/settings.py
@@ -0,0 +1,29 @@
+import os
+from envparse import env
+
+MONGO_HOST = env.str('MONGO_HOST', default="")
+MONGO_PORT = env.int('MONGO_PORT', default=27017)
+MONGO_DBNAME = env.str('MONGO_DBNAME', default="")
+
+RESOURCE_METHODS = ['GET', 'POST']
+
+DOMAIN = {
+	'user': {
+		'additional_lookup': {
+            'url': 'regex("[\w]+")',
+            'field': 'username',
+        },
+    	'schema': {
+            'username': {
+               	'type': 'string',
+               	'unique': True
+            },
+            'password': {
+               	'type': 'string'
+            },
+            'phone': {
+               	'type': 'string'
+            }
+        }
+    }
+}
